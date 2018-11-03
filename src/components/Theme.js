8c398d3cf775212224e26c4d311d9786ba6f9e9b
Neilphpbeginner <lemmer.neil@gmail.com>
2018-11-03 14:07:37 +0200

    Adding theme js file

---
+++ src/components/Theme.js
@@ -0,0 +1,12 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+export default createMuiTheme({
+    palette: {
+        primary: {
+            main: '#103452'
+        },
+        secondary: {
+            main: '#FFFFFF'
+        },
+    },
+});
